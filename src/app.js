d1d9c9037dba50d5305f2290cc17174a669e49c2
JJMoraless <jjmorales.dev@gmail.com>
2023-08-28 09:37:14 -0500

    feat: :sparkles: introducida ruta de login y users

--- src/app.js
+++ src/app.js
@@ -5,17 +5,23 @@ import clienteRouter from "./routes/clienteRouter.js";
 import productoRouter from "./routes/productoRouter.js";
 import repartidorRouter from "./routes/repartidorRouter.js";
 import restauranteRouter from "./routes/restauranteRouter.js";
-
+import loginRouter from './routes/authRouter.js';
+import userRouter from './routes/userRouter.js'
+import morgan from 'morgan'
+import "./utils/auth/index.js"
 
 dotenv.config();
 const app = express();
 app
 .use(express.json())
+.use(morgan('dev'))
 .use('/pedidos', pedidoRouter)
 .use('/clientes', clienteRouter)
 .use('/restaurantes', restauranteRouter)
 .use('/productos', productoRouter)
-.use('/repartidors', repartidorRouter);
+.use('/repartidors', repartidorRouter)
+.use('/login', loginRouter)
+.use('/users', userRouter )
 
 let config = JSON.parse(process.env.MY_SERVER);
 app.listen(config, () => {
