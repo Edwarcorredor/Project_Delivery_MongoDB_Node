56cec518957a931c6901403fb4b070bc25992688
JJMoraless <jjmorales.dev@gmail.com>
2023-08-28 09:46:38 -0500

    feat: :sparkles: protegiendo la ruta de pedidos con roles

--- src/routes/pedidoRouter.js
+++ src/routes/pedidoRouter.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
 import ControllerCrud from "../controllers/ControllerCrud.js";
+import passport from "passport";
+import { checkRoles } from "../middlewares/auth.middleware.js";
 
 const pedidoRouter = Router();
+pedidoRouter.use(passport.authenticate("jwt", { session: false }));
 
 pedidoRouter
-            
-.get('/todos', ControllerCrud.getAll)
-.post('/uno/', ControllerCrud.setDocument)
-.put('/update/:id', ControllerCrud.updateDocument)
-.delete('/eliminar/:id', ControllerCrud.deleteDocument);
+.get("/todos", checkRoles("cliente"), ControllerCrud.getAll)
+.post("/uno/", ControllerCrud.setDocument)
+.put("/update/:id", ControllerCrud.updateDocument)
+.delete("/eliminar/:id", ControllerCrud.deleteDocument);
 
 export default pedidoRouter;
