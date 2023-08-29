659a340981f834120c2d20c0b8af0359ce38cdb4
JJMoraless <jjmorales.dev@gmail.com>
2023-08-28 22:04:51 -0500

    feat: :sparkles: protegido la ruta repartidor

--- src/routes/repartidorRouter.js
+++ src/routes/repartidorRouter.js
@@ -1,13 +1,23 @@
 import { Router } from "express";
 import ControllerCrud from "../controllers/ControllerCrud.js";
+import { checkRoles } from "../middlewares/auth.middleware.js";
+import passport from "passport";
 
 const repartidorRouter = Router();
 
 repartidorRouter
-            
-.get('/todos', ControllerCrud.getAll)
-.post('/uno/', ControllerCrud.setDocument)
-.put('/update/:id', ControllerCrud.updateDocument)
-.delete('/eliminar/:id', ControllerCrud.deleteDocument);
+  .use(passport.authenticate("jwt", { session: false }))
+  .get(
+    "/todos",
+    checkRoles("user", "cliente", "vendedor"),
+    ControllerCrud.getAll
+  )
+  .post("/uno/", checkRoles("repartidor", "user"), ControllerCrud.setDocument)
+  .put("/update/:id", ControllerCrud.updateDocument)
+  .delete(
+    "/eliminar/:id",
+    checkRoles("repartidor", "admin"),
+    ControllerCrud.deleteDocument
+  );
 
 export default repartidorRouter;
