d7115070e47be8c810c7aa4ace1a70e76390cae3
JJMoraless <jjmorales.dev@gmail.com>
2023-08-28 22:06:32 -0500

    feat: :sparkles: protegiendo el modulo de clientes con roles y passport

--- src/routes/pedidoRouter.js
+++ src/routes/pedidoRouter.js
@@ -4,12 +4,13 @@ import passport from "passport";
 import { checkRoles } from "../middlewares/auth.middleware.js";
 
 const pedidoRouter = Router();
-pedidoRouter.use(passport.authenticate("jwt", { session: false }));
+
 
 pedidoRouter
+.use(passport.authenticate("jwt", { session: false }))
 .get("/todos", checkRoles("cliente"), ControllerCrud.getAll)
-.post("/uno/", ControllerCrud.setDocument)
-.put("/update/:id", ControllerCrud.updateDocument)
-.delete("/eliminar/:id", ControllerCrud.deleteDocument);
+.post("/uno/", checkRoles("cliente"), ControllerCrud.setDocument)
+.put("/update/:id",checkRoles("cliente") ,ControllerCrud.updateDocument)
+.delete("/eliminar/:id", checkRoles("cliente")  , ControllerCrud.deleteDocument);
 
 export default pedidoRouter;
