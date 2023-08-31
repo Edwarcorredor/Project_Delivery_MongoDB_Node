5a2fe757242a8afd9edd13b60bc548acd664a724
EdwarDaniloCorredorAmayaCAMPUS <eddacoam7.2@gmail.com>
2023-08-31 15:21:01 -0500

    feat: :sparkles: Limit login
    Se crea la funcion para el limite de login

--- src/config/limit.js
+++ src/config/limit.js
@@ -1,8 +1,23 @@
 import rateLimit from "express-rate-limit";
-export let limitPet = ()=>{
+export const limitPet = ()=>{
     return rateLimit({
         windowMs: 30 * 1000,
         max: 10,
+        standardHeaders: true, 
+        legacyHeaders: false, 
+        message: (req,res)=>{
+            res.status(429).send({
+                status: 429, 
+                message: "Limite alcanzado"
+            });
+        }
+    })    
+}
+
+export const limitLogin = ()=>{
+    return rateLimit({
+        windowMs: 30 * 1000,
+        max: 5,
         standardHeaders: true, 
         legacyHeaders: false, 
         message: (req,res)=>{
