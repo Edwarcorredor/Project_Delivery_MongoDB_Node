4eea403a7a8fee2f0ed2ad48a9a08f948674aa5a
EdwarDaniloCorredorAmayaCAMPUS <eddacoam7.2@gmail.com>
2023-08-31 15:19:55 -0500

    feat: :sparkles: Limite de peticiones
    Se crea la funcion par manejar el limite de peticiones

---
+++ src/config/limit.js
@@ -0,0 +1,15 @@
+import rateLimit from "express-rate-limit";
+export let limitPet = ()=>{
+    return rateLimit({
+        windowMs: 30 * 1000,
+        max: 10,
+        standardHeaders: true, 
+        legacyHeaders: false, 
+        message: (req,res)=>{
+            res.status(429).send({
+                status: 429, 
+                message: "Limite alcanzado"
+            });
+        }
+    })    
+}
