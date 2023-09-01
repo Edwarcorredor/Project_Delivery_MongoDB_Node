86f24b02cb4884c72671d31bb6119247e969228e
EdwarDaniloCorredorAmayaCAMPUS <eddacoam7.2@gmail.com>
2023-09-01 16:52:38 -0500

    feat: :sparkles: Version de autorizacion

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import { login } from "../controllers/AuthController.js";
 import passport from "passport";
-
+import routesVersioning  from 'express-routes-versioning';
 const routerLogin = Router();
+const version = routesVersioning();
 
 routerLogin.post(
   "/",
   passport.authenticate("local", { session: false }),
-  login
-);
-// router.post("/logout", logOut);
+  version({
+    "1.0.0": login
+}));
 
 export default routerLogin;
