2d82207bb2078ce8da640f63b0cacd216db62744
EdwarDaniloCorredorAmayaCAMPUS <eddacoam7.2@gmail.com>
2023-09-01 09:20:39 -0500

    feat: :sparkles: Version API
    Se crea la configuracion para manejar version api

--- src/utils/auth/strategies/jwt.strategy.js
+++ src/utils/auth/strategies/jwt.strategy.js
@@ -3,9 +3,14 @@ import { Strategy, ExtractJwt } from "passport-jwt";
 const options = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
   secretOrKey: process.env.JWT_SECRET,
+  passReqToCallback: true
 };
 
-const jwtStrategy = new Strategy(options, (payload, done) => {
+const jwtStrategy = new Strategy(options, (req, payload, done) => {
+  console.log(payload.permisos);
+  if(!payload.permisos.includes(req.headers["accept-version"])){
+    return done(null,false);
+  }
   return done(null, payload);
 });
 
