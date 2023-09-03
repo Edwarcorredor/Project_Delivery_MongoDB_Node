7f6c59e0378c803ccb3b3c0f2e9070ea0b2edbe5
EdwarDaniloCorredorAmayaCAMPUS <eddacoam7.2@gmail.com>
2023-09-03 14:19:15 -0500

    refactor: :art: Cambio de plantilla para conexion atlas

--- src/config/atlas.js
+++ src/config/atlas.js
@@ -3,7 +3,7 @@ import { MongoClient } from 'mongodb';
 dotenv.config("../");
 export async function conexion() {
   try {
-    const uri = `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASSWORD}@edwarcorredor.zavkvu2.mongodb.net/${process.env.ATLAS_DB}`;
+    const uri = `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASSWORD}@cluster0.owv3sij.mongodb.net/${process.env.ATLAS_DB}`;
     const options = {
       useNewUrlParser: true,
       useUnifiedTopology: true,
