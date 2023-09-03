fff32af8d901709c2d6b431e25d9754b258718ab
JJMoraless <jjmorales.dev@gmail.com>
2023-09-02 21:16:41 -0500

    feat: :sparkles: rutas con proteccion y versionado a restaurante

--- src/routes/restauranteRouter.js
+++ src/routes/restauranteRouter.js
@@ -1,28 +1,72 @@
 import { Router } from "express";
-import ControllerCrud from "../controllers/ControllerUser.js";
-import { checkRoles } from "../middlewares/auth.middleware.js";
 import passport from "passport";
+import { checkRoles } from "../middlewares/auth.middleware.js";
 import RestauranteCrll from "../controllers/RestauranteController.js";
-import routesVersioning  from 'express-routes-versioning';
-
+import routesVersioning from "express-routes-versioning";
+import { validateShemas } from "../middlewares/validateShemas.middlware.js";
+import { restauranteSchema } from "../dto/modelDTO.js";
 const version = routesVersioning();
-
 const restauranteRouter = Router();
 
 restauranteRouter
   .use(passport.authenticate("jwt", { session: false }))
-  .get("/", checkRoles("vendedor", "cliente"), RestauranteCrll.getByCategory)
+
   .get(
-    "/todos",
-    checkRoles("vendedor", "cliente"),
-    RestauranteCrll.getRestaurantes
+    "/mis_restaurantes",
+    checkRoles("vendedor"),
+    version({
+      "1.0.0": RestauranteCrll.getRestaurantesByVendedor,
+    })
   )
-  .post("/uno/", checkRoles("vendedor"), RestauranteCrll.postRestaurante)
-  .put("/update/:id", checkRoles("vendedor"), ControllerCrud.updateDocument)
+
+  .get(
+    "/mis_restaurantes/:id/total_pedidos",
+    checkRoles("vendedor"),
+    version({
+      "1.0.0": RestauranteCrll.getTotalPedidosRestaurante,
+    })
+  )
+
+  .get(
+    "/:id/productos",
+    checkRoles("vendedor", "cliente", "repartidor"),
+    version({
+      "1.0.0": RestauranteCrll.getProducts,
+    })
+  )
+
+  .get(
+    "/categorias",
+    checkRoles("cliente", "vendedor"),
+    version({
+      "1.0.0": RestauranteCrll.getByCategoryAll,
+    })
+  )
+
+  .post(
+    "/uno",
+    checkRoles("vendedor"),
+    validateShemas(restauranteSchema),
+    version({
+      "1.0.0": RestauranteCrll.postRestauranteByVendedor,
+    })
+  )
+
+  .put(
+    "/update/:id",
+    checkRoles("vendedor"),
+    validateShemas(restauranteSchema),
+    version({
+      "1.0.0": RestauranteCrll.updateRestauranteByVendedor,
+    })
+  )
+
   .delete(
     "/eliminar/:id",
     checkRoles("vendedor"),
-    ControllerCrud.deleteDocument
+    version({
+      "1.0.0": RestauranteCrll.deleteRestauranteByVendedor,
+    })
   );
 
 export default restauranteRouter;
