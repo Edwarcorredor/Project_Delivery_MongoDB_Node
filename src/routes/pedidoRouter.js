61c2e7f8fa42535e58609115832639df99466377
JJMoraless <jjmorales.dev@gmail.com>
2023-09-02 21:15:15 -0500

    feat: :sparkles: agregada las rutas con proteccion de pedidos

--- src/routes/pedidoRouter.js
+++ src/routes/pedidoRouter.js
@@ -3,30 +3,48 @@ import ControllerCrud from "../controllers/ControllerUser.js";
 import passport from "passport";
 import { checkRoles } from "../middlewares/auth.middleware.js";
 import PedidosCrll from "../controllers/PedidosController.js";
-import routesVersioning  from 'express-routes-versioning';
+import routesVersioning from "express-routes-versioning";
+import { validateShemas } from "../middlewares/validateShemas.middlware.js";
+import { pedidoSchema } from "../dto/modelDTO.js";
 
 const version = routesVersioning();
 
 const pedidoRouter = Router();
 
 pedidoRouter
-.use(passport.authenticate("jwt", { session: false }))
-.get("/uno", checkRoles("cliente"), PedidosCrll.get)
+  .use(passport.authenticate("jwt", { session: false }))
+  .get(
+    "/",
+    checkRoles("cliente"),
+    version({
+      "1.0.0": PedidosCrll.getByCliente,
+    })
+  )
 
-.get("/todos",checkRoles("repartidor"), version({
-    "1.0.0": ControllerCrud.getAll
-}))
+  .get(
+    "/todos",
+    checkRoles("repartidor"),
+    version({
+      "1.0.0": PedidosCrll.getAll,
+    })
+  )
 
-.post("/uno/", checkRoles("cliente"), version({
-    "1.0.0": ControllerCrud.setDocument
-})) 
+  .post(
+    "/uno",
+    checkRoles("cliente", "vendedor"),
+    validateShemas(pedidoSchema),
+    version({
+      "1.0.0": PedidosCrll.postByCliente,
+    })
+  )
 
-.put("/update/:id",checkRoles("cliente") , version({
-    "1.0.0": ControllerCrud.updateDocument
-})) 
-
-.delete("/eliminar/:id", checkRoles("cliente")  , version({
-    "1.0.0": ControllerCrud.deleteDocument
-}))
+  .put(
+    "/update/:id",
+    checkRoles("cliente"),
+    validateShemas(pedidoSchema),
+    version({
+      "1.0.0": PedidosCrll.updateByCliente,
+    })
+  );
 
 export default pedidoRouter;
